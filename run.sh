3e15e5b7919f86dc16b699a66fdc106b4e340636
Pierre Mavro <pmavro@nousmotards.com>
2017-03-12 02:28:09 +0100

    Adding more information on run

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -e
-
-echo WGET_OPTIONS ${CURL_OPTIONS}
+echo WGET_OPTIONS ${WGET_OPTIONS}
 echo URL ${URL}
-echo APP_NAME ${APP_NAME}
-echo ENVIRONEMENT ${ENVIRONEMENT}
+echo ENVIRONMENT ${ENVIRONMENT}
 echo APP_NAME ${APP_NAME}
 echo VERSION ${VERSION}
-echo JAVA_OPTIONS ${JAVA_OPTIONS}
+echo JAVA_OPTIONS ${JAVA_OPTIONS:-}
 
 # Default download method
-wget -O /home/nm-user/app.jar ${WGET_OPTIONS} ${URL}/${APP_NAME}/${ENVIRONEMENT}/${APP_NAME}_${VERSION}.jar
+echo "Downloading jar..."
+wget -q -O /home/nm-user/app.jar ${WGET_OPTIONS} ${URL}/${APP_NAME}/${ENVIRONMENT}/${APP_NAME}_${VERSION}.jar
 # Fallback download method
 if [ $? != 0 ] ; then
     echo "Using alternative solution to download app"
 fi
 
-su - nm-user -c java ${JAVA_OPTIONS} /home/nm-user/app.jar
+echo "Launching jar..."
+su - nm-user -c "java ${JAVA_OPTIONS} -jar /home/nm-user/app.jar"
